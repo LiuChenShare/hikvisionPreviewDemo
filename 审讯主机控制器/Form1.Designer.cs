4c33f65a991150966473031684658c7a30aa43fd
村长 <757891685@qq.com>
2019-11-28 15:44:07 +0800

    加入审讯主机控制器

---
+++ 审讯主机控制器/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace 审讯主机控制器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(442, 14);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 12);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "登录";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(236, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "密码";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "用户名";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(234, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "设备端口";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "设备IP";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxPassword.Location = new System.Drawing.Point(308, 65);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(112, 21);
+            this.textBoxPassword.TabIndex = 37;
+            this.textBoxPassword.Text = "iflytek!";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(78, 65);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(114, 21);
+            this.textBoxUserName.TabIndex = 36;
+            this.textBoxUserName.Text = "admin";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(308, 19);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(112, 21);
+            this.textBoxPort.TabIndex = 35;
+            this.textBoxPort.Text = "8000";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(78, 19);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(114, 21);
+            this.textBoxIP.TabIndex = 34;
+            this.textBoxIP.Text = "10.5.23.234";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(435, 33);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(78, 50);
+            this.btnLogin.TabIndex = 33;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Device IP";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(234, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(234, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Device Port";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 50);
+            this.button1.TabIndex = 43;
+            this.button1.Text = "开始";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "审讯主机控制器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 审讯主机控制器/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 审讯主机控制器
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 登录的摄像头
+        /// </summary>
+        private Int32 m_lUserID = -1;
+        private Int32 m_lRealHandle = -1;
+        /// <summary>
+        /// 正在录制
+        /// </summary>
+        private bool Recording = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxIP.Text == "" || textBoxPort.Text == "" ||
+                textBoxUserName.Text == "" || textBoxPassword.Text == "")
+            {
+                MessageBox.Show("请输入IP、端口、用户名和密码！");
+                return;
+            }
+            if (m_lUserID < 0)
+            {
+                string DVRIPAddress = textBoxIP.Text; //设备IP地址或者域名
+                Int16 DVRPortNumber = Int16.Parse(textBoxPort.Text);//设备服务端口号
+                string DVRUserName = textBoxUserName.Text;//设备登录用户名
+                string DVRPassword = textBoxPassword.Text;//设备登录密码
+
+                CHCNetSDK.NET_DVR_DEVICEINFO_V30 DeviceInfo = new CHCNetSDK.NET_DVR_DEVICEINFO_V30();
+
+                //登录设备 Login the device
+                m_lUserID = CHCNetSDK.NET_DVR_Login_V30(DVRIPAddress, DVRPortNumber, DVRUserName, DVRPassword, ref DeviceInfo);
+                if (m_lUserID < 0)
+                {
+                    var iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    var str = "NET_DVR_Login_V30 failed, error code= " + iLastErr; //登录失败，输出错误号
+                    MessageBox.Show(str);
+                    return;
+                }
+                else
+                {
+                    //登录成功
+                    MessageBox.Show("Login Success!");
+                    btnLogin.Text = "Logout";
+                }
+
+            }
+            else
+            {
+                //注销登录 Logout the device
+                if (m_lRealHandle >= 0)
+                {
+                    MessageBox.Show("请先停止预览");
+                    return;
+                }
+
+                if (!CHCNetSDK.NET_DVR_Logout(m_lUserID))
+                {
+                    var iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    var str = "NET_DVR_Logout failed, error code= " + iLastErr;
+                    MessageBox.Show(str);
+                    return;
+                }
+                m_lUserID = -1;
+                btnLogin.Text = "Login";
+            }
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m_lUserID < 0)
+            {
+                MessageBox.Show("请先登录设备");
+                return;
+            }
+
+            if (!Recording)
+            {
+                if (!CHCNetSDK.NET_DVR_StartDVRRecord(m_lUserID, 0xffff, 0))
+                {
+                    var iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    var str = "开始录制异常, error code= " + iLastErr;
+                    MessageBox.Show(str);
+                    return;
+                }
+                button1.Text = "停止";
+                Recording = true;
+            }
+            else
+            {
+                if (!CHCNetSDK.NET_DVR_StopDVRRecord(m_lUserID, 0xffff))
+                {
+                    var iLastErr = CHCNetSDK.NET_DVR_GetLastError();
+                    var str = "停止录制异常, error code= " + iLastErr;
+                    MessageBox.Show(str);
+                    return;
+                }
+                button1.Text = "开始";
+                Recording = false;
+            }
+        }
+    }
+}
